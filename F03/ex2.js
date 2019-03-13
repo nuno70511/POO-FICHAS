f6527a529825deebc77d5fc464f8f7e3a7980745
Nuno <9170511@esmad.ipp.pt>
2019-03-13 14:38:21 +0000

    F03 ex2

---
+++ F03/ex2.js
@@ -0,0 +1,8 @@
+//b.
+document.querySelector("#p1").innerHTML = "Está um dia de chuva";
+
+//c.
+document.querySelector("img").src = "https://www.altominho.tv/site/wp-content/uploads/2019/02/chuva.jpg";
+
+//d.
+document.querySelector("#p2").style = "background-color: yellow;";
