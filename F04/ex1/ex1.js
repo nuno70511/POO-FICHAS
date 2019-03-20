2f3e399114d5a4b9dffc81cefb5feca8c5adb54d
Nuno <9170511@esmad.ipp.pt>
2019-03-20 12:10:06 +0000

    F04 ex1

---
+++ F04/ex1/ex1.js
@@ -0,0 +1,15 @@
+//a.
+    //i.
+    const user = {};
+
+    //ii.
+    user.name = "John";
+
+    //iii.
+    user.surname = "Smith";
+
+    //iv.
+    user.name = "Pete";
+
+    //v.
+    delete user.name;
